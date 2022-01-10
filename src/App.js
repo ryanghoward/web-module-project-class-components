0026a3b3003e81b1a502553cf7b1427cb9ca51f5
Ryan Howard <ryanghoward@protonmail.com>
2022-01-10 15:01:43 -0800

    Minor clean up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React from 'react';
 
-const thingsToDo = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-];
-
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -21,8 +8,19 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      thingsToDo: thingsToDo,
-    };
+      tasksToDo: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ] 
+    }
   }
 
   render() {
